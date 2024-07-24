15312b75fe397d138f6d6ad430cef632ce93c8c3
fiyigp gp <fiyigp@gmail.com>
2024-07-24 19:28:05 +0000

    feat: add first verification

--- element.sh
+++ element.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
+
+if [[ $1 ]]
+  then
+  #search element
+  else
+  echo "Please provide an element as an argument.";
+fi
