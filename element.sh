45468fa39d05d68e70d4237a3428a14d8bcf9ff4
fiyigp gp <fiyigp@gmail.com>
2024-07-24 19:30:17 +0000

    feat: add search element code

--- element.sh
+++ element.sh
@@ -3,7 +3,16 @@ PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 
 if [[ $1 ]]
   then
-  #search element
+    ELEMENT_RESULT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number = $1 OR name = '$1' OR symbol = '$1';")
+  if [[ -z $ELEMENT_RESULT ]]
+    then
+    echo "I could not find that element in the database."
+    else
+    echo "$ELEMENT_RESULT" | while read ATOMIC_NUMBER BAR NAME BAR SYMBOL BAR TYPE BAR ATOMIC_MASS BAR MELTING_POINT_CELSIUS BAR BOILING_POINT_CELSIUS
+    do
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
+    done
+  fi
   else
   echo "Please provide an element as an argument.";
 fi
