2a147f35369dc59b789f04d6fa1209e478086aa7
fiyigp gp <fiyigp@gmail.com>
2024-07-24 19:32:59 +0000

    fix: add verification for atomic_number

--- element.sh
+++ element.sh
@@ -3,7 +3,12 @@ PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 
 if [[ $1 ]]
   then
-    ELEMENT_RESULT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number = $1 OR name = '$1' OR symbol = '$1';")
+  if [[ $1 =~ ^[0-9]+$ ]]
+    then
+    ELEMENT_RESULT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number = $1;")
+    else
+    ELEMENT_RESULT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE name = '$1' OR symbol = '$1';")
+  fi
   if [[ -z $ELEMENT_RESULT ]]
     then
     echo "I could not find that element in the database."
